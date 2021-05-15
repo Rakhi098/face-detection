61b42bec7c5e3cd9e593b4cd992e0b11c5943b7a
Rakhi098 <83413213+Rakhi098@users.noreply.github.com>
2021-05-15 16:21:06 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np
+img = cv.imread('group.jpg')
+cv.imshow('img', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+haar_cascade = cv.CascadeClassifier(r'C:\Users\Amit\PycharmProjects\project_work\PycharmProjects\opencv\haar_face.xml')
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+print(f'Number of faces found = {len(faces_rect)}')
+for (x, y, w, h) in faces_rect:
+    cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+
+cv.imshow('detected faces', img)
+cv.waitKey(0)
+
